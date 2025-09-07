4739629a25861a1fab169339433076c61ddafdce
muqimjon <muqimjon.mr@gmail.com>
2025-09-07 12:07:42 +0500

    Changed destination src file

---
+++ src/backend/Forex.Infrastructure/Persistence/Migrations/20250907041903_AddEmailProp.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+
+namespace Forex.Infrastructure.Persistence.Migrations;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+/// <inheritdoc />
+public partial class AddEmailProp : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+}
