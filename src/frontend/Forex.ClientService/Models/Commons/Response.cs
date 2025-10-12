d6700c6abc14872523ea222faeb36ffc401156b0
muqimjon <muqimjon.mr@gmail.com>
2025-10-12 21:23:18 +0500

    same changes

--- src/frontend/Forex.ClientService/Models/Commons/Response.cs
+++ src/frontend/Forex.ClientService/Models/Commons/Response.cs
@@ -4,11 +4,8 @@ using System.Text.Json.Serialization;
 
 public class Response<T>
 {
-    [JsonPropertyName("data")]
     public T? Data { get; set; }
-    [JsonPropertyName("message")]
     public string Message { get; set; } = string.Empty;
-    [JsonPropertyName("statusCode")]
     public int StatusCode { get; set; }
 
     [JsonIgnore]
