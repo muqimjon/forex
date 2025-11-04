8f9859a480b3c7684adf8fc44f91ed50ff462a5e
avazbekm <siddikovavazbek213@gmail.com>
2025-11-04 14:04:33 +0500

    added processPageViewModel

---
+++ src/frontend/Forex.Wpf/Pages/Processes/ProcessPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Forex.Wpf.Pages.Processes;
+
+using Forex.Wpf.Pages.Home;
+using Forex.Wpf.Pages.Processes.ViewModels;
+using Forex.Wpf.Pages.Products.ViewModels;
+using Forex.Wpf.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using System.Windows.Controls;
+
+
+/// <summary>
+/// Interaction logic for ProcessPage.xaml
+/// </summary>
+public partial class ProcessPage : Page
+{
+    private static MainWindow Main => (MainWindow)Application.Current.MainWindow;
+    private ProcessPageViewModel vm;
+    public ProcessPage()
+    {
+        InitializeComponent();
+        vm = App.AppHost!.Services.GetRequiredService<ProcessPageViewModel>();
+        DataContext = vm;
+
+    }
+
+    private void BtnBack_Click(object sender, RoutedEventArgs e)
+    {
+        if (NavigationService?.CanGoBack == true)
+            NavigationService.GoBack();
+        else
+            Main.NavigateTo(new HomePage());
+    }
+
+}
