64d6d624c189156e66899250b466684093c7a583
avazbekm <siddikovavazbek213@gmail.com>
2025-12-01 11:16:55 +0500

    filled buttons with diffirient colors in reports

---
+++ src/frontend/Forex.Wpf/Pages/Reports/ViewModels/DebtorCreditorReportView.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Forex.Wpf.Pages.Reports.ViewModels;
+using System.Windows.Controls;
+
+
+/// <summary>
+/// Interaction logic for DebtorCreditorReportView.xaml
+/// </summary>
+public partial class DebtorCreditorReportView : UserControl
+{
+    public DebtorCreditorReportView()
+    {
+        InitializeComponent();
+    }
+}
